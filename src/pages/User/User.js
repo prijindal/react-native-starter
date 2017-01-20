9bbba6c3c2f1868feee8de4ecf58a03b3469e51b
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-01-20 16:52:36 +0530

    Added Image to user page

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,7 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import { View, Text, Image } from 'react-native';
 
+import theme from '../../themes/base-theme';
 import Layout from '../../components/Layout';
+
+const styles = {
+  avatar: {
+    height: 240,
+    width: 360,
+  },
+  toolbar: {
+    backgroundColor: theme.transparent,
+  },
+};
 
 class User extends Component {
   static propTypes = {
@@ -15,18 +26,22 @@ class User extends Component {
   }
 
   render() {
+    const { title, avatar, body } = this.props.user;
     return (
-      <Layout
-        enableBackButton
-        title="Home"
-        actions={[]}
-        onActionSelected={this.onActionSelected}
-        navigator={this.props.navigator}
-      >
-        <View>
-          <Text>{this.props.user.title}</Text>
-        </View>
-      </Layout>
+      <View>
+        <Image source={{ uri: avatar }} style={styles.avatar}>
+          <Layout
+            enableBackButton
+            title={title}
+            titleColor={theme.darkText}
+            toolbarStyle={styles.toolbar}
+            actions={[]}
+            onActionSelected={this.onActionSelected}
+            navigator={this.props.navigator}
+          />
+        </Image>
+        <Text style={styles.body}>{body}</Text>
+      </View>
     );
   }
 }
